c16ca444b6389f4a7c1e1814a0a735801ac7d275
mfung <mjfung1@gmail.com>
2021-03-25 00:51:26 -0400

    difficult one 3/5 come back to it"

---
+++ bloomberg/invalidTransactions1169.js
@@ -0,0 +1,37 @@
+var isValid = function(transaction, map) {
+    const [name, time, amount, city] = transaction.split(',');
+    
+    if (amount > 1000) return true;
+    
+    const attributes = map[name];
+    
+    for (const attribute of attributes) {
+        if (city !== attribute.citys && Math.abs(time - attribute.times) <= 60) return true;
+    };
+    return false;
+};
+
+var invalidTransactions = function(transactions) {
+    const invalid = [];
+    const mapTransactions = {};
+    
+    for (const transaction of transactions) {
+        
+        const [name, time, amount, city] = transaction.split(',');
+        
+        if (name in mapTransactions) {
+            mapTransactions[name].push({times: time, citys: city});
+        } else {
+            mapTransactions[name] = [{times: time, citys: city}];
+        };
+    };
+
+    
+    for (const transaction of transactions) {
+        if (isValid(transaction, mapTransactions)) {
+            invalid.push(transaction);
+        }
+    };
+    console.log(mapTransactions)
+    return invalid;
+};
