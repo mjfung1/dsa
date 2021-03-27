8b5270f8956ba8de4999a35d13d7d4f179c168de
mfung <mjfung1@gmail.com>
2021-03-26 21:25:42 -0400

    another easy problem

---
+++ 771jewelsStones.js
@@ -0,0 +1,9 @@
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+    
+    for (let stone of stones) {
+        if (jewels.includes(stone)) count++;
+    }
+    
+    return count;
+};
