81e052390e6d47007265738ab749432dd08344bd
mfung <mjfung1@gmail.com>
2021-04-12 13:41:44 -0400

    kk

---
+++ bloomberg/394decodeString.js
@@ -0,0 +1,42 @@
+
+// 394. Decode String
+
+var decodeString = function(s) {
+    const freq = [];
+    let tempFreq = '';
+    
+    const chars = [];
+    let decoded = '';
+    
+    
+    // s = "3[a2[c]]"
+    for (const char of s) {
+        
+        if (!isNaN(char)) {
+            tempFreq += char //`${tempFreq}${char}`;
+            
+        } else if (char === '[') {
+            freq.push(tempFreq);
+            tempFreq = '';
+            
+            
+            chars.push(decoded);
+            decoded = '';
+        
+            
+        } else if (char === ']') {
+            //start decoding
+            decoded = chars.pop() + decoded.repeat(freq.pop());
+            
+        } else {
+            decoded += char;
+        }
+        
+    }
+    
+    
+    
+    
+    
+    return decoded;
+};
--- findSum.js
+++ findSum.js
@@ -1,19 +1,31 @@
-const findSum = (arr, target) => {
-    const output = []; 
-    const wantHash = {};
+// const findSum = (arr, target) => {
+//     const output = []; 
+//     const wantHash = {};
 
-    for (const num of arr) {
-        wantHash[num] = true;
+//     for (const num of arr) {
+//         wantHash[num] = true;
         
-    }
+//     }
     
-    for (const num of arr) {
-        let want = target - num;
-        if (want in wantHash) {
-            output.push([num, want])
+//     for (const num of arr) {
+//         let want = target - num;
+//         if (want in wantHash) {
+//             output.push([num, want])
+//         }
+//     }
+//     return output;
+// }
+
+const findSum = (arr, target) => {
+    const output= [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) output.push([arr[i], arr[j]])
         }
     }
     return output;
 }
 
-console.log(findSum([1,1,4,5,3,5,2,6,6], 7))
+
+// console.log(findSum([1,1,4,5,3,5,2,6,6], 7))
+console.log(findSum([1,2,1,1,3], 3))
