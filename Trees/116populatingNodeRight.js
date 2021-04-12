fc7cf9ff89e0b1a6fad982b47b9df6b767c574e0
mfung <mjfung1@gmail.com>
2021-04-12 14:24:04 -0400

    simple bfs

---
+++ Trees/116populatingNodeRight.js
@@ -0,0 +1,27 @@
+// 116. Populating Next Right Pointers in Each Node
+
+
+var connect = function(root) {
+    if (!root) return root;
+    const queue = [root];
+    
+    while (queue.length) {
+        let size = queue.length;
+        
+        while (size--) {
+            
+            const item = queue.shift();
+            if (size === 0) {
+                item.next = null;
+                
+            } else {
+                item.next = queue[0]
+            }
+            
+            if (item.left) queue.push(item.left)
+            if (item.right) queue.push(item.right)
+
+        }
+    }
+    return root;
+};
