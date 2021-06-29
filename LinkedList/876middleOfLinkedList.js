7fd0ba4aaf400d8629d87e9d7e5cccd068539f0f
mfung <mjfung1@gmail.com>
2021-06-29 00:29:51 -0400

    linked list practice

---
+++ LinkedList/876middleOfLinkedList.js
@@ -0,0 +1,14 @@
+// 876. Middle of the Linked List
+
+
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
