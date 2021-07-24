19fe4ebad3b73b1e0aa17b444fa2d9df0df1c7be
mfung <mjfung1@gmail.com>
2021-07-24 19:23:22 -0400

    347

---
+++ priorityQueue/347topKFrequentElements.js
@@ -0,0 +1,27 @@
+
+// 347. Top K Frequent Elements
+
+
+var topKFrequent = function (nums, k) {
+  const freq = {};
+  for (const num of nums) {
+    freq[num] = freq[num] + 1 || 1;
+  }
+
+  let maxPQ = new MaxPriorityQueue();
+
+  for (let num in freq) {
+    maxPQ.enqueue(num, freq[num]);
+  }
+
+  const output = [];
+
+  while (maxPQ.size() && k--) {
+    let node = maxPQ.dequeue();
+    output.push(node.element);
+  }
+
+  return output;
+};
+
+
